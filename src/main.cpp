5338be30c46167e4e2aa2d791fdc6fefc99352f2
Westboarder44 <lunaschulze2006@gmx.de>
2026-06-24 20:54:11 +0200

    Third-Party Integration complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#define VOLK_IMPLEMENTATION
+#include <volk.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <spdlog/spdlog.h>
 
 int main() {
-    std::cout << "Simulengine Initialized" << std::endl;
+    spdlog::info("Simulengine: Third-party integration test.");
+
+    if (!glfwInit()) {
+        spdlog::error("Failed to initialize GLFW");
+        return -1;
+    }
+
+    if (volkInitialize() != VK_SUCCESS) {
+        spdlog::error("Failed to initialize Volk");
+        return -1;
+    }
+
+    spdlog::info("Libraries initialized successfully.");
+    
+    glfwTerminate();
     return 0;
 }
